255ce44aaddddf75821de8ba2142cd35e2865dab
Greg Kempe <greg@kempe.net>
2015-07-16 18:50:46 +0200

    Add missing dependency on bs4

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'mammoth >= 0.3.19'
+        'mammoth >= 0.3.19',
+        'beautifulsoup4==4.4.0',
     ],
 
     # List additional groups of dependencies here (e.g. development
