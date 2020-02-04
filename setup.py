4b85bfe5ceb4137d3ad8884965c9583859d160e5
Delena Malan <delena.malan@gmail.com>
2020-02-04 16:54:43 +0200

    Remove future dependency

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ setup(
     install_requires=[
         'mammoth >= 0.3.19, < 0.3.31',
         'beautifulsoup4 >= 4.4.0',
-        'future',
     ],
 
     # List additional groups of dependencies here (e.g. development
