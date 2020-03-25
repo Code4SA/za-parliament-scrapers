48aafe0dd55a47cf37aa5e79ed14dec6df82e8d3
Delena Malan <delena.malan@gmail.com>
2020-03-25 09:35:51 +0200

    Looks like the number should be part of the regex
    The doctests in questions.py for `extract_questions_from_text` expects
    the number to be part of the intro.

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -55,7 +55,7 @@ class QuestionAnswerScraperTest(unittest.TestCase):
         assert_equal(1, len(questions))
         assert_equal('Ms D Kohler', questions[0]['askedby'])
         assert_equal('Minister of Police', questions[0]['questionto'])
-        assert_equal('Ms D Kohler (DA) to ask the Minister of Police:', questions[0]['intro'])
+        assert_equal('126. Ms D Kohler (DA) to ask the Minister of Police:', questions[0]['intro'])
         assert_equal('With reference to the reply to question 228 on 18 March 2014, (a) how much discontinued ammunition is still in circulation in the SA Police Service in each province and (b) what action is to be taken to remove this ammunition from use?', questions[0]['question'])
 
     def test_extract_questions_from_text_old_format_without_number_before_asker_name(self):
@@ -72,6 +72,6 @@ class QuestionAnswerScraperTest(unittest.TestCase):
         questions = self.scraper.extract_questions_from_text(text)
         assert_equal(1, len(questions))
         assert_equal('Ms S J Graham', questions[0]['askedby'])
-        assert_equal('Ms S J Graham (DA) asked the Minister of Public Works and Infrastructure:', questions[0]['intro'])
+        assert_equal('104. Ms S J Graham (DA) asked the Minister of Public Works and Infrastructure:', questions[0]['intro'])
         assert_equal('Minister of Public Works and Infrastructure', questions[0]['questionto'])
         assert_equal('What is the number of residential properties owned by her department in the Lephalale Local Municipality which are vacant?', questions[0]['question'])
